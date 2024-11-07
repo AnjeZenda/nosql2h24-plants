a5c8a2040978fc0661aca49e6dcc91cb6b5267d6
LeraChernyakova <2215391@mail.ru>
2024-11-07 23:50:42 +0300

    added new route and some changes

--- client/src/router.js
+++ client/src/router.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     { path: '/', redirect: '/plants/start'},
     { path: '/plants/start', name: 'Start', component: () => import('./components/pages/StartPage.vue')},
-    { path: '/plants/main', name: 'Main', component: () => import('./components/pages/MainPage.vue')}
+    { path: '/plants/sale', name: 'Sale', component: () => import('./components/pages/SalePlantPage.vue')},
+    { path: '/plants/care', name: 'Care', component: () => import('./components/pages/CarePage.vue')}
 ]
 
 const router = createRouter({
