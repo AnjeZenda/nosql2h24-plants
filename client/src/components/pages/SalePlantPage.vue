9659d4a2a77f1aa75c94812ca47c5cd4745b7b30
LeraChernyakova <2215391@mail.ru>
2024-11-07 23:06:11 +0300

    added REST to page with sale plant

--- client/src/components/pages/SalePlantPage.vue
+++ client/src/components/pages/SalePlantPage.vue
@@ -1,26 +1,68 @@
 <template>
-  <TopAppBar />
+  <Navbar />
   <div class="plant-container">
     <div class="plant-grid">
-      <div v-for="(plant, index) in paginatedPlants" :key="index" class="plant-card">
+      <div v-for="plant in plants" class="plant-card">
         <div class="plant-content">
-          <img v-if="plant.img" :src="plant.img" alt="Plant Image" class="plant-image" />
+          <img v-if="plant.image" :src="plant.image" alt="Plant Image" class="plant-image" />
           <div class="plant-info">
-            <div v-if="plant.title" class="plant-title">{{ plant.title }}</div>
-            <div v-if="plant.price" class="plant-price">{{ plant.price }}</div>
+            <div v-if="plant.species" class="plant-title">{{ plant.species }}</div>
+            <div v-if="plant.price" class="plant-price">{{ formatPrice(plant.price) }}</div>
             <div v-if="plant.place" class="plant-place">{{ plant.place }}</div>
-            <div v-if="plant.created_at" class="plant-date">{{ plant.created_at }}</div>
+            <div v-if="plant.createdAt" class="plant-date">{{ formatDate(plant.createdAt) }}</div>
           </div>
         </div>
       </div>
     </div>
-    <div class="pagination">
-      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
-      <span>Page {{ currentPage }} of {{ totalPages }}</span>
-      <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
-    </div>
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+import axios from "axios";
+
+const PLANTS_URL = '/api/plants';
+
+export default {
+  name: "Sale",
+  components: { Navbar },
+
+  data() {
+    return {
+      plants: [],
+    };
+  },
+
+  mounted() {
+    this.getPlants();
+  },
+
+  methods: {
+    async getPlants() {
+      const response = await axios.get(PLANTS_URL);
+      this.plants = response.data.map(elem => ({
+        image: elem.image,
+        species: elem.species,
+        price: elem.price,
+        createdAt: elem.createdAt,
+        place: elem.place
+      }));
+    },
+
+    formatDate(date) {
+      return new Date(date).toLocaleDateString("ru-RU", {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      });
+    },
+
+    formatPrice(price) {
+      return `${price} ₽`;
+    }
+  }
+}
+</script>
 
 <style scoped>
 .plant-container {
@@ -70,43 +112,4 @@
 .plant-date {
   color: #7E7E7E;
 }
-
-.pagination {
-  margin-top: 10px;
-}
 </style>
-
-<script setup>
-import { ref, computed, onMounted } from 'vue';
-import TopAppBar from "@/components/Navbar.vue";
-import plantsData from '../../assets/plants_sell.json';
-
-const plants = ref([]);
-const currentPage = ref(1);
-const pageSize = 15;
-
-const totalPages = computed(() => {
-  return Math.ceil(plants.value.length / pageSize);
-});
-
-const paginatedPlants = computed(() => {
-  const start = (currentPage.value - 1) * pageSize;
-  return plants.value.slice(start, start + pageSize);
-});
-
-const nextPage = () => {
-  if (currentPage.value < totalPages.value) {
-    currentPage.value++;
-  }
-};
-
-const prevPage = () => {
-  if (currentPage.value > 1) {
-    currentPage.value--;
-  }
-};
-
-onMounted(() => {
-  plants.value = plantsData;
-});
-</script>
