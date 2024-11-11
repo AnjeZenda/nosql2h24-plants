045ee5187751ac1666dcb61151bb411a23125c94
LeraChernyakova <2215391@mail.ru>
2024-11-11 12:33:32 +0300

    added some changes to work with API

--- client/src/router.js
+++ client/src/router.js
@@ -4,7 +4,8 @@ const routes = [
     { path: '/', redirect: '/plants/start'},
     { path: '/plants/start', name: 'Start', component: () => import('./components/pages/StartPage.vue')},
     { path: '/plants/sale', name: 'Sale', component: () => import('./components/pages/SalePlantPage.vue')},
-    { path: '/plants/care', name: 'Care', component: () => import('./components/pages/CarePage.vue')}
+    { path: '/plants/care', name: 'Care', component: () => import('./components/pages/CarePage.vue')},
+    { path: '/plants/user', name: 'User', component: () => import('./components/pages/UserPage.vue')}
 ]
 
 const router = createRouter({
