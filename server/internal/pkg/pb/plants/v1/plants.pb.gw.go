46c34ebf2949d3a1c723e420e42352a9cf6782ce
DmitryChuban <metro2033dg@gmail.com>
2024-12-18 11:51:35 +0300

    gen

--- server/internal/pkg/pb/plants/v1/plants.pb.gw.go
+++ server/internal/pkg/pb/plants/v1/plants.pb.gw.go
@@ -751,6 +751,24 @@ func local_request_PlantsAPI_GetArchivedPlantsV1_0(ctx context.Context, marshale
 
 }
 
+func request_PlantsAPI_ListPlantTypesV1_0(ctx context.Context, marshaler runtime.Marshaler, client PlantsAPIClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetPlantTypesV1Request
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.ListPlantTypesV1(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_PlantsAPI_ListPlantTypesV1_0(ctx context.Context, marshaler runtime.Marshaler, server PlantsAPIServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetPlantTypesV1Request
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.ListPlantTypesV1(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterPlantsAPIHandlerServer registers the http handlers for service PlantsAPI to "mux".
 // UnaryRPC     :call PlantsAPIServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -1030,6 +1048,29 @@ func RegisterPlantsAPIHandlerServer(ctx context.Context, mux *runtime.ServeMux, 
 		}
 
 		forward_PlantsAPI_GetArchivedPlantsV1_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_PlantsAPI_ListPlantTypesV1_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_PlantsAPI_ListPlantTypesV1_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PlantsAPI_ListPlantTypesV1_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1314,6 +1355,26 @@ func RegisterPlantsAPIHandlerClient(ctx context.Context, mux *runtime.ServeMux, 
 
 	})
 
+	mux.Handle("GET", pattern_PlantsAPI_ListPlantTypesV1_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PlantsAPI_ListPlantTypesV1_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PlantsAPI_ListPlantTypesV1_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1341,6 +1402,8 @@ var (
 	pattern_PlantsAPI_GetActivePlantsV1_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "plants", "active", "userId"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_PlantsAPI_GetArchivedPlantsV1_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"api", "plants", "archive", "userId"}, "", runtime.AssumeColonVerbOpt(true)))
+
+	pattern_PlantsAPI_ListPlantTypesV1_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"api", "plants", "types"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
@@ -1367,4 +1430,6 @@ var (
 	forward_PlantsAPI_GetActivePlantsV1_0 = runtime.ForwardResponseMessage
 
 	forward_PlantsAPI_GetArchivedPlantsV1_0 = runtime.ForwardResponseMessage
+
+	forward_PlantsAPI_ListPlantTypesV1_0 = runtime.ForwardResponseMessage
 )
